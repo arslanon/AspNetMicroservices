3fc22c5b9180b1a6ada413eeab2b2199fd1ea1b2
Mutlu Boz <mutluboz@Mutlus-MacBook-Pro.local>
2021-10-31 15:10:07 +0300

    Add shopping aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string username)
+        {
+            var response = await _client.GetAsync($"/api/v1/basket/{username}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
